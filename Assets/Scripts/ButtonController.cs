f95285598a3b3165e7c92b59e92d0a8578a87b52
jessicard <jessicard@github.com>
2016-03-20 21:03:27 -0700

    Add keyboard input and sound

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+	public void StartGame(){
+		Application.LoadLevel("MiniGame");
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+}
---
+++ Assets/Scripts/KeyboardInputListener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardInputListener : MonoBehaviour {
+
+	void OnGUI () {
+		if (Event.current.type == EventType.KeyDown) {
+			if (Input.GetKey(KeyCode.Escape))
+			{
+				Application.LoadLevel("StartMenu");
+			}
+		}
+	}
+
+}
